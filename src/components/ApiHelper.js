cbc70dc7419c1f337450932911897319d0c653ca
Ashik19aug <ashikshafayet@gmail.com>
2024-12-18 21:56:36 +0600

    open sandbox and payment success alert done

---
+++ src/components/ApiHelper.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+export const RequestPostAPI = async (formData, config) => {
+
+  let axiosConfig = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'multipart/form-data',
+      'Accept' : 'multipart/form-data',
+      'Authorization' : 'Bearer test',
+      'action': 'submitCheckoutDummy',
+      'apiUrl': '/api/v1/submitCheckoutDummy',
+      'sendOtp' : 'application/json',
+      'deviceId' : 'DEVICE_01229911223',
+      'googleAdId' : 'GOOGLE0909323',
+      'subscriptionId' : 'SUBSCRIBE090932',
+      'userAgent' : 'getUA',
+      'browserName' : 'browserName',
+      'browserVersion' : 'browserVersion',
+      'simId' : 'SIM321122111',
+      'osPlatform' : 'osName',
+      'deviceType' : 'deviceType',
+      'imeiNo' : '950345832533',
+    }
+  };
+  axiosConfig.headers = {...axiosConfig.headers,...config};
+
+  const result = await axios.post('http://localhost/signup-api/public/api/v1/endpoint', formData, axiosConfig)
+    .then((res) => {
+      // console.log('---', res)
+      if(res.data.code === 200){
+        return res;
+      } else if(res.data.code === 422){
+        return res;
+      } else if(res.data.code === 401){
+        return res;
+      }
+    })
+    .catch((err) => {
+      return err;
+    })
+  return result;
+};
--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import {RequestPostAPI} from "./ApiHelper.js";
+import {useLocation} from "react-router-dom";
 
 const PaymentForm = () => {
+  const [showAlert, setShowAlert] = useState(false);
   const [fileSelected, setFileSelected] = useState(null);
+  const [trnsDataShow, setTrnsDataShow] = useState(false);
   const tableData = [
     {
       no: 1,
@@ -26,9 +30,50 @@ const PaymentForm = () => {
     const file = e.target.files[0];
     setFileSelected(file);
   };
+  const LoadData = () => {
+    setTrnsDataShow(true);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      'action': 'submitCheckoutDummy',
+      'apiUrl': '/api/v1/submitCheckoutDummy',
+      'lang': 'en',
+    };
+    const formData = new FormData();
+    let apiResponse =  await RequestPostAPI(formData, config);
+    console.log('handleSubmit', apiResponse)
+    if(apiResponse?.data?.code === 200){
+      if(apiResponse?.data?.data?.data?.GatewayPageURL){
+        window.open(apiResponse?.data?.data?.data?.GatewayPageURL);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const currentUrl = window.location.href;
+    if (currentUrl === "http://localhost:5173/upload-file-information") {
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 3000);
+    }
+  }, []);
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
+      {/*Alert Section Start*/}
+      {/* Popup Notification */}
+      {showAlert && (
+        <div
+          className="fixed top-4 right-3 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-lg shadow-md">
+          <div className="flex items-center">
+            <span className="font-bold mr-2">Success!</span>
+            <span>Your payment was successfully processed!</span>
+          </div>
+        </div>
+      )}
+      {/*Alert Section End*/}
       <div className="bg-white rounded-xl shadow-lg w-full max-w-4xl flex overflow-hidden">
         {/* Left Section */}
         <div className="w-1/3 bg-gray-300 p-8 flex flex-col items-center justify-center">
@@ -42,14 +87,10 @@ const PaymentForm = () => {
             <span className="font-light">Support Email ID : Support@gmail.com</span>
           </p>
         </div>
-
         {/* Right Section */}
         <div className="w-2/3 p-8 flex flex-col justify-center">
-          <h3 className="text-lg font-normal mb-6 text-gray-800 text-center justify-start">
-            Payment Form
-          </h3>
+          <h3 className="text-lg font-normal mb-6 text-gray-800 text-center justify-start">Payment Form</h3>
           {/*<form>*/}
-
             <div className="mb-4">
               <label
                 htmlFor="file"
@@ -65,16 +106,15 @@ const PaymentForm = () => {
                   onChange={handleFileChange}
                 />
                 <button
-                  type="button"
+                  type="button" onClick={()=>LoadData()}
                   className="bg-blue-600  text-sm text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700 transition duration-200"
                 >
                   Upload
                 </button>
               </div>
             </div>
-
             {/* Table Section */}
-            {fileSelected && (
+            {trnsDataShow && (
               <div className="mt-6">
                 <h3 className="text-xs font-semibold mb-4">Application(s) List</h3>
                 <table className="w-full border-collapse border border-gray-300">
@@ -110,21 +150,18 @@ const PaymentForm = () => {
                 </table>
               </div>
             )}
-
-
             {/* Button */}
-            {fileSelected && (
+            {trnsDataShow && (
               <div className="flex justify-center">
-                <button type="submit" className="w-full text-sm bg-blue-600 text-white p-3 mt-4 rounded-lg shadow-md hover:bg-blue-700 transition duration-200 flex justify-center items-center">
+                <button type="submit" onClick={handleSubmit} className="w-full text-sm bg-blue-600 text-white p-3 mt-4 rounded-lg shadow-md hover:bg-blue-700 transition duration-200 flex justify-center items-center">
                   Proceed to Pay
                   <span className="ml-2">➡</span>
                 </button>
               </div>
             )}
           {/*</form>*/}
-
           {/* Footer */}
-          {fileSelected && (
+          {trnsDataShow && (
             <div className="mt-6 text-center text-gray-500 text-sm">
               Powered by{' '}
               <span className="text-blue-600 font-semibold">SSLCommerz</span>
